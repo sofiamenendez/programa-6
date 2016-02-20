088bec7513364ed414cfa61792cc3c3a647f1f77
sofiamenendez <samenendez97@unitec.edu>
2016-02-20 16:24:59 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int n1,n2,suma;
+int main()
+{
+    cout << "ingresar el numero #1..:";
+    cin >>(n1); //scanf("%d", &nl)
+     cout << "ingresar el numero #2..:";
+    cin >>(n2); //scanf("%d", &n2)
+    suma= n1+n2;
+    cout<<"La suma Final es "<<suma<<"\n";
+
+    return 0;
+}
